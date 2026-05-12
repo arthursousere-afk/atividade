42e03cf9ba3da136d981597f86d7bebab5dbf257
arthur <arthursousere@gmail.com>
2026-05-12 11:43:24 -0300

    t

---
+++ atividade.py
@@ -0,0 +1,8 @@
+computador = input("esse computador e: ")
+if computador in "do capeta" "encapetado" "do diabo" "sem alma" "ruim":
+    print("que deus tenha misericordia")
+else:
+    print("voce e mentiroso ou nao usou ele ainda")
+
+    
+
